b09992d4dc1ff2581f6de6bbd29b11362b419453
matishocke <dashmati4@gmail.com>
2025-02-14 11:36:11 +0100

    de

--- Lexias.Services.WarehouseAPI/Models/Product.cs
+++ Lexias.Services.WarehouseAPI/Models/Product.cs
@@ -11,7 +11,7 @@ namespace Lexias.Services.WarehouseAPI.Models
         public string? Description { get; set; }
         public decimal? Price { get; set; }
         public int StockQuantity { get; set; }
-        public string? Category { get; set; } // Example: "Men", "Women", "Accessories", etc.
+        public string? Category { get; set; } // Example: "Men", "Women", "Accessories", etc.fr
         //public List<string>? Sizes { get; set; } // Example: ["S", "M", "L", "XL"]
         public string? Color { get; set; }
         public ItemType? ItemType { get; set; }
